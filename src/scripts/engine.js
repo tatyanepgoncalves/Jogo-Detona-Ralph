730875b12d53fe12d1815ab294bceeb90bbbfd21
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-10-02 09:55:08 -0300

    feat: add function play sound

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -29,6 +29,12 @@ function countDown() {
   }
 }
 
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.2; 
+  audio.play();
+}
+
 function randomSquare() {
   state.view.squares.forEach((square) => {
     square.classList.remove("enemy");
@@ -48,6 +54,7 @@ function addListenerHitBox() {
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
+        playSound("hit");
       }
     });
   });
